6bea49570b579d852ed98522bd10ad8bb4ef1c49
AndresOlarte <deuterocanonico@gmail.com>
2020-03-07 10:29:19 +0100

    init

---
+++ src/main/java/com/example/SpringPortsAndAdapters/SpringPortsAndAdaptersApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPortsAndAdapters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPortsAndAdaptersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPortsAndAdaptersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringPortsAndAdapters/SpringPortsAndAdaptersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPortsAndAdapters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPortsAndAdaptersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
